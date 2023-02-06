0d4cc887f7c19abb400a58abf600dc60f6d9a55a
Pujan_gc <pujan.choudhury@bybox.com>
2023-02-06 22:43:03 +0000

    ExpenseManagerApi partial changes
     #Please enter the commit message for your changes. Lines starting

---
+++ ExpenseManager.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ExpenseManager.Tests.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ExpenseManagerDb/ExpenseManagerDbSql.sql
@@ -0,0 +1,13 @@
+-- Database: ExpenseManagerDb
+
+-- DROP DATABASE IF EXISTS "ExpenseManagerDb";
+
+CREATE DATABASE "ExpenseManagerDb"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
